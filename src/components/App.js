726c65775e08a6735998d4aa190f2e4f50297243
was.li <wasuwat.se@gmail.com>
2018-12-29 14:02:01 +0700

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import { h, Component } from 'preact'
+import PlaneBig from '../images/plane_big.jpg'
+import PlaneSmall from '../images/plane_small.jpg'
 
 export default class App extends Component {
   render() {
@@ -9,6 +11,18 @@ export default class App extends Component {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero optio tempora odio voluptatum numquam, blanditiis dignissimos, ad harum cum magnam
           nulla nostrum aspernatur accusantium quaerat? Totam maxime nulla harum esse!
         </p>
+        <picture>
+          <source media="(max-width: 425px)" srcset={PlaneSmall} />
+          <source media="(min-width: 426px)" srcset={PlaneBig} />
+          <img src={PlaneSmall} alt="Plane" />
+        </picture>
+        <img
+          src={PlaneSmall}
+          srcSet={`${PlaneSmall} 200w, ${PlaneBig} 767w`}
+          sizes="(max-width: 425px) 200px,
+            (min-width: 426px) 767px"
+          alt="Plane"
+        />
       </main>
     )
   }
